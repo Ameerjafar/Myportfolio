25d5ec34ab3f0bf9332f108d3b9c54b4f973cdd9
Ameer jafar <ameerjafar123@gmail.com>
2024-03-17 22:33:53 +0530

    adding code

--- frontend/src/component/ProjectComponent.jsx
+++ frontend/src/component/ProjectComponent.jsx
@@ -3,13 +3,13 @@ export function ProjectComponent() {
         <div>
             <div className = 'flex justify-center p-3 text-2xl font-semibold'>Projects</div>
             <div className = 'flex justify-center gap-3'>
-                <div className = 'bg-gray-200 h-44 rounded-lg'>
+                <div className = 'bg-gray-200 h-44 rounded-lg font-bold'>
                     <a href = "https://github.com/Ameerjafar/BlogApp.git">This is blog app</a>
                 </div>
-                <div>
+                <div className = 'bg-gray-200 h-44 rounded-lg font-bold'>
                     <a href = "https://github.com/Ameerjafar/ExpenseTracker-Frontend.git">This is expanse tracker website</a>
                 </div>
-                <div>
+                <div className = 'bg-gray-200 h-44 rounded-lg font-bold '>
                     <a href = 'https://github.com/Ameerjafar/paytm-clone.git'>This is paytm clone application</a>
                 </div>
             </div>
