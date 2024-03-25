b3265b925d2ef6bb8e08beafb5e5fe525f5fa32d
Ameer jafar <ameerjafar123@gmail.com>
2024-03-25 10:59:50 +0530

    making changes

--- frontend/src/component/ProjectComponent.jsx
+++ frontend/src/component/ProjectComponent.jsx
@@ -9,7 +9,7 @@ export function ProjectComponent() {
                 <div className = 'bg-gray-200 h-44 rounded-lg font-bold'>
                     <a href = "https://github.com/Ameerjafar/ExpenseTracker-Frontend.git">This is expanse tracker website</a>
                 </div>
-                <div className = 'bg-gray-200 h-44 rounded-lg font-bold '>
+                <div className = 'bg-gray-200 hover:bg-gray-300 h-44 rounded-lg font-bold'>
                     <a href = 'https://github.com/Ameerjafar/paytm-clone.git'>This is paytm clone application</a>
                 </div>
             </div>
